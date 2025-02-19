ebbfee5a201943bf4d90aabdeb9031e10d16b2d1
Vismaya6 <vismayatheresabenny2028@cs.sjcetpalai.ac.in>
2025-02-19 15:42:03 +0530

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,20 @@
+/*Author:Vismaya Theresa Benny
+c program to reverse a string
+*/
+#include<stdio.h>
+int main (){
+char str1[20];
+int length;
+printf("Enter a string:");
+fgets(str1,sizeof(str1),stdin);
+for(length=0;str1[length]!='\0';length++);
+int i=length-1;
+for(int j=0;j<length/2;j++){
+char temp=str1[i];
+str1[i]=str1[j];
+str1[j]=temp;
+i--;
+}
+printf("The reverse string is %s",str1);
+return 0;
+}
